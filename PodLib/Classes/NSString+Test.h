fd3d7e5432a35a8a121ab7c96304841ba1f07ff1
lijingbiao <dinglei@syswin.com>
2019-04-03 15:59:47 +0800

    第一次提交

---
+++ PodLib/Classes/NSString+Test.h
@@ -0,0 +1,12 @@
+//
+//  NSString+Test.h
+//  PodLib
+//
+//  Created by syswin on 2019/4/3.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (Test)
+- (void)test;
+@end
---
+++ PodLib/Classes/NSString+Test.m
@@ -0,0 +1,14 @@
+//
+//  NSString+Test.m
+//  PodLib
+//
+//  Created by syswin on 2019/4/3.
+//
+
+#import "NSString+Test.h"
+
+@implementation NSString (Test)
+- (void)test{
+    NSLog(@"只是一个测试");
+}
+@end
